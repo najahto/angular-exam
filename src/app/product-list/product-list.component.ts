52c468e027d7df763c6e83958c3f97a2284686ca
Taha Omar NAJAH <tahaomarnajah@gmail.com>
2020-02-27 13:34:16 +0100

    list produt

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,15 +1,37 @@
+import { Produit } from './../Modules/produit/produit.module';
+import { Router } from '@angular/Router';
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
+import { ProduitServiceService } from '../services/produit-service.service';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
+  templateUrl: './product-list.component.html', 
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+  listData = new MatTableDataSource<Produit>();
+  displayedColumns: string[] = ['nom', 'nom_court', 'prix_de_bas', 'prix_de_vente', 'remise', 'quantite_initial', 'quantite_en_stock', 'Actions'];
 
-  constructor() { }
+  constructor( private service : ProduitServiceService,private router:Router) { }
 
   ngOnInit() {
   }
+
+  fetchElements()
+  {
+    this.service.getProducts().subscribe(
+      res => {
+        if (!res) return;
+        console.log(res);
+        this.listData = new MatTableDataSource(res as any);
+      }
+    )
+  }
+  applyFilter(filterValue: string) {
+    this.listData.filter = filterValue.trim().toLowerCase();
+  }
+  
+
 
 }
